a9375257684a3300f8079467810fadaa994dd34e
willvelida <willvelida@hotmail.co.uk>
2021-04-07 20:27:14 +1200

    Initial cut

---
+++ MyHealth.Common/MyHealth.Common/AzureBlobHelpers.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHealth.Common
+{
+    public class AzureBlobHelpers : IAzureBlobHelpers
+    {
+        public BlobContainerClient ConnectToBlobClient(string connectionString, string containerName)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            return containerClient;
+        }
+
+        public async Task<Stream> DownloadBlobAsStreamAsync(BlobContainerClient containerClient, string blobName)
+        {
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+            var stream = new MemoryStream();
+
+            await blobClient.DownloadToAsync(stream);
+
+            return stream;
+        }
+
+        public async Task UploadBlobAsync(BlobContainerClient containerClient, string blobName, string fileName)
+        {
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+            await blobClient.UploadAsync(fileName);
+        }
+    }
+}
---
+++ MyHealth.Common/MyHealth.Common/IAzureBlobHelpers.cs
@@ -0,0 +1,37 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHealth.Common
+{
+    public interface IAzureBlobHelpers
+    {
+        /// <summary>
+        /// Connects to a BlobClient in Azure Blob Storage
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <param name="containerName"></param>
+        /// <returns></returns>
+        BlobContainerClient ConnectToBlobClient(string connectionString, string containerName);
+
+        /// <summary>
+        /// Uploads a blob to Azure Storage
+        /// </summary>
+        /// <param name="containerClient"></param>
+        /// <param name="blobName"></param>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        Task UploadBlobAsync(BlobContainerClient containerClient, string blobName, string fileName);
+
+        /// <summary>
+        /// Downloads the content of a specified blob to a stream
+        /// </summary>
+        /// <param name="containerClient"></param>
+        /// <param name="blobName"></param>
+        /// <returns></returns>
+        Task<Stream> DownloadBlobAsStreamAsync(BlobContainerClient containerClient, string blobName);
+    }
+}
