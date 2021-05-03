96d6e951802b2a68a1fd277e08957f8435b00ae2
willvelida <willvelida@hotmail.co.uk>
2021-05-03 16:21:37 +1200

    Adding initial cut for KeyVault helpers'

---
+++ MyHealth.Common/MyHealth.Common/IKeyVaultHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace MyHealth.Common
+{
+    public interface IKeyVaultHelper
+    {
+        Task SaveSecretToKeyVaultAsync(string secretName, string secretValue);
+        Task RetrieveSecretFromKeyVaultAsync(string secret);
+    }
+}
---
+++ MyHealth.Common/MyHealth.Common/KeyVaultHelper.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System;
+using System.Threading.Tasks;
+
+namespace MyHealth.Common
+{
+    public class KeyVaultHelper : IKeyVaultHelper
+    {
+        private readonly SecretClient _secretClient;
+
+        public KeyVaultHelper(string keyVaultName)
+        {
+            var keyVaultUri = "https://" + keyVaultName + ".vault.azure.net";
+            _secretClient = new SecretClient(new Uri(keyVaultUri), new DefaultAzureCredential());
+        }
+
+        public async Task RetrieveSecretFromKeyVaultAsync(string secret)
+        {
+            await _secretClient.GetSecretAsync(secret);
+        }
+
+        public async Task SaveSecretToKeyVaultAsync(string secretName, string secretValue)
+        {
+            await _secretClient.SetSecretAsync(secretName, secretValue);
+        }
+    }
+}
