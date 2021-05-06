71b7776773723466e878ab5d0f4abfd4baa8e7aa
willvelida <willvelida@hotmail.co.uk>
2021-05-06 21:09:58 +1200

    Adding Nutrition models

---
+++ MyHealth.Common/MyHealth.Common/Models/Nutrition.cs
@@ -0,0 +1,14 @@
+﻿namespace MyHealth.Common.Models
+{
+    public class Nutrition
+    {
+        public string NutritionDate { get; set; }
+        public int Calories { get; set; }
+        public double Carbs { get; set; }
+        public double Fat { get; set; }
+        public double Fiber { get; set; }
+        public double Protein { get; set; }
+        public int Sodium { get; set; }
+        public int WaterInMl { get; set; }
+    }
+}
---
+++ MyHealth.Common/MyHealth.Common/Models/NutritionEnvelope.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace MyHealth.Common.Models
+{
+    public class NutritionEnvelope
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        public Nutrition Nutrition { get; set; }
+        public string DocumentType { get; set; }
+    }
+}
