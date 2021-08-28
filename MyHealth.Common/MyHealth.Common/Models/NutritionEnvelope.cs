0341c3def16ed254f6d62c9b47adb55bfc6dec8a
willvelida <willvelida@hotmail.co.uk>
2021-08-29 10:20:51 +1200

    Coverting Date properties in envelopes to string type

--- MyHealth.Common/MyHealth.Common/Models/NutritionEnvelope.cs
+++ MyHealth.Common/MyHealth.Common/Models/NutritionEnvelope.cs
@@ -8,7 +8,7 @@ namespace MyHealth.Common.Models
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
         public Nutrition Nutrition { get; set; }
-        public DateTime Date { get; set; }
+        public string Date { get; set; }
         public string DocumentType { get; set; }
     }
 }
